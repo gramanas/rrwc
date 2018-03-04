7a365364ec3efd480e5e7794a53a62e20f730ae5
gramanas <anastasis.gramm2@gmail.com>
2018-03-04 13:38:00 +0200

    Add licence info

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -179,7 +179,22 @@ void MainWindow::actionHelp() {
     QDesktopServices::openUrl(QUrl("https://github.com/gramanas/rrwc/wiki"));
 }
 void MainWindow::actionAbout() {
-    QMessageBox::information(this, "About rrwc...", "Version 1.0\nΆντε και καλά ξεσκάρτ!");
+  QMessageBox msgBox(this);
+  msgBox.setWindowTitle("About rrwc...");
+  msgBox.setTextFormat(Qt::RichText);   //this is what makes the links clickable
+  msgBox.setText("Version 1.0<br>Άντε και καλά ξεσκάρτ!<br><br>\
+rrwc  Copyright (C) 2018  Anastasis Grammenos<br>\
+This program comes with ABSOLUTELY NO WARRANTY.<br>\
+This is free software, and you are welcome to redistribute it<br>\
+under certain conditions.<br>\
+See <a href='https://www.gnu.org/licenses/gpl-3.0.en.html'>GPLv3</a> for details.");
+  msgBox.exec();
+//   QMessageBox::information(this, "About rrwc...", "Version 1.0\nΆντε και καλά ξεσκάρτ!\n\n\
+// rrwc  Copyright (C) 2018  Anastasis Grammenos\n\
+// This program comes with ABSOLUTELY NO WARRANTY.\n\
+// This is free software, and you are welcome to redistribute it\n\
+// under certain conditions;\
+// see <a href='https://www.gnu.org/licenses/gpl-3.0.en.html'>GPLv3</a> for details.");
 }
 
 void MainWindow::actionSaveProfile() {
